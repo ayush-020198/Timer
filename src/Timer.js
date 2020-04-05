d6585a29144ab2f5ffd1d649b4893d1f2a474cc9
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-04-05 20:42:12 +0530

    timer app first commit

---
+++ src/Timer.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class Timer extends React.Component {
+
+componentDidMount(){
+  this.timer = setInterval(this.ticktock, 1000)
+}
+
+ticktock(){
+  this.setState({clock: new Date() - this.props.start});
+}
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      clock:0
+    };
+    this.ticktock = this.ticktock.bind(this);
+  }
+
+  render () {
+    var time = Math.round(this.state.clock/1000);
+  return(
+     <div className="Timer">
+      <header>You wasted your</header>
+      <span>{time}</span><br />
+      <footer>Seconds</footer>
+     </div>
+  );
+  }
+}
+
+export default Timer;
