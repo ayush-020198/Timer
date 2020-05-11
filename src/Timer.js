b2ec75efbf45944f10512c8aee603cbb4e517ae9
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-05-11 15:47:38 +0530

    second commit

--- src/Timer.js
+++ src/Timer.js
@@ -1,33 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Timer extends React.Component {
+  componentDidMount() {
+    this.timer = setInterval(this.ticktock, 1000);
+  }
 
-componentDidMount(){
-  this.timer = setInterval(this.ticktock, 1000)
-}
+  ticktock() {
+    this.setState({ clock: new Date() - this.props.start });
+  }
 
-ticktock(){
-  this.setState({clock: new Date() - this.props.start});
-}
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      clock:0
+      clock: 0,
     };
     this.ticktock = this.ticktock.bind(this);
   }
 
-  render () {
-    var time = Math.round(this.state.clock/1000);
-  return(
-     <div className="Timer">
-      <header>You wasted your</header>
-      <span>{time}</span><br />
-      <footer>Seconds</footer>
-     </div>
-  );
+  render() {
+    //It makes converts the timer to seconds through algorithm
+    var time = Math.round(this.state.clock / 1000);
+    return (
+      <div className="Timer">
+        <header>You wasted your</header>
+        <span>{time}</span>
+        <br />
+        <footer>Seconds</footer>
+      </div>
+    );
   }
 }
 
